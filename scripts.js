a79a80f41c10325f5c3330f395b56a98abf7e06e
catscatsforever <44739966+catscatsforever@users.noreply.github.com>
2025-05-25 06:31:21 +0000

    ADD Full Changelog, Content diff tracker

--- scripts.js
+++ scripts.js
@@ -256,26 +256,19 @@ document.addEventListener("DOMContentLoaded", async function () {
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => bootstrap.Tooltip.getOrCreateInstance(tooltipTriggerEl, {title: get_translation(current_language, tooltipTriggerEl.getAttribute("data-bs-title")), trigger: 'hover'}))
 
 
-    content_mapping.push({item_id: 'PATCH_FULL_CHANGELOG', view_id: 'view_5', strings: {title: 'TXT_KEY_PATCH_FULL_CHANGELOG_TITLE'}, diff: getPatchDiff()})
+    content_mapping.push({item_id: 'PATCH_FULL_CHANGELOG', view_id: 'view_4', strings: {title: 'TXT_KEY_PATCH_FULL_CHANGELOG_TITLE', text: 'TXT_KEY_PATCH_FULL_CHANGELOG_SUMMARY'}}, {item_id: 'PATCH_CONTENT_DIFF', view_id: 'view_5', strings: {title: 'TXT_KEY_PATCH_CONTENT_DIFF_TITLE'}, diff: getPatchDiff()})
     let latestVersion = '0'
     content_mapping.push(...Object.entries(patchNotes.ru.versions).map(([k, v]) => {latestVersion = k; return {item_id: `PATCH${k}`, view_id: 'view_4', strings: {title: `TXT_KEY_PATCH_${k}_TITLE`, text: `TXT_KEY_PATCH_${k}_SUMMARY`}}}))
 
     articleList = {...Object.entries(content_mapping.filter(x => x?.strings.title).map((item) => {return {label: item.strings.title, id: item.item_id}})).reduce((acc, [k, v]) => {return {...acc, [get_translation('en', v.label).toLowerCase()]: v.id, [get_translation('ru', v.label).toLowerCase()]: v.id}}, {})}
     data_mappings.categories[0].sections.push(
-        /*{
-            label: 'TXT_KEY_PATCH_FULL_TITLE',
-            items: Object.entries(patchNotes.ru.full).map(([k, v]) => {return {id: `PATCH_${k}`, label: `TXT_KEY_PATCH_${k}_TITLE`}})
-        },*/
         {
             label: 'TXT_KEY_PATCH_VERSIONS_TITLE',
-            items: Object.entries(patchNotes.ru.versions).map(([k, v]) => {return {id: `PATCH${k}`, label: `TXT_KEY_PATCH_${k}_TITLE`}}).sort((a, b) => -a.label.localeCompare(b.label, undefined, {numeric: true}))
+            items: [{id: `PATCH_FULL_CHANGELOG`, label: `TXT_KEY_PATCH_FULL_CHANGELOG_TITLE`}, {id: `PATCH_CONTENT_DIFF`, label: `TXT_KEY_PATCH_CONTENT_DIFF_TITLE`}].concat(Object.entries(patchNotes.ru.versions).map(([k, v]) => {return {id: `PATCH${k}`, label: `TXT_KEY_PATCH_${k}_TITLE`}}).sort((a, b) => -a.label.localeCompare(b.label, undefined, {numeric: true})))
         })
-    console.log('latest', latestVersion)
     localStorage.setItem('latestVersion', latestVersion);
-    translations['en'] = {...translations['en'], ...patchTxtTags.en, ...Object.entries(patchNotes.en.versions).reduce((acc, [k, v]) => {return {...acc, [`TXT_KEY_PATCH_${k}_TITLE`]: `Version ${k}`, [`TXT_KEY_PATCH_${k}_SUMMARY`]: '<ul>' + addMarkup(v) + '</ul>'}}, {})}
-    translations['ru'] = {...translations['ru'], ...patchTxtTags.ru, ...Object.entries(patchNotes.ru.versions).reduce((acc, [k, v]) => {return {...acc, [`TXT_KEY_PATCH_${k}_TITLE`]: `Версия ${k}`, [`TXT_KEY_PATCH_${k}_SUMMARY`]: '<ul>' + addMarkup(v) + '</ul>'}}, {})}
-    if (!search_article(location.hash.substring(1))) {
-        currentTopic++
+    translations['en'] = {...translations['en'], ...patchTxtTags.en, ...Object.entries(patchNotes.en.versions).reduce((acc, [k, v]) => {return {...acc, [`TXT_KEY_PATCH_${k}_TITLE`]: `Version ${k}`, [`TXT_KEY_PATCH_${k}_SUMMARY`]: '<ul>' + addMarkup(v) + '</ul>'}}, {}), TXT_KEY_PATCH_FULL_CHANGELOG_SUMMARY: '<ul>' + addMarkup(patchNotes.en.full) + '</ul>'}
+    translations['ru'] = {...translations['ru'], ...patchTxtTags.ru, ...Object.entries(patchNotes.ru.versions).reduce((acc, [k, v]) => {return {...acc, [`TXT_KEY_PATCH_${k}_TITLE`]: `Версия ${k}`, [`TXT_KEY_PATCH_${k}_SUMMARY`]: '<ul>' + addMarkup(v) + '</ul>'}}, {}), TXT_KEY_PATCH_FULL_CHANGELOG_SUMMARY: '<ul>' + addMarkup(patchNotes.ru.full) + '</ul>'}
     if (!search_article(location.hash.substring(1), true)) {
         currentTopic = 0
         listOfTopicsViewed.push(get_info_from_item_id('PEDIA_HOME_PAGE'))
